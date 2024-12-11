c327226204e838fa871215b634e520b4e3131812
Moosky-1 <m.navti@gmail.com>
2024-12-11 20:28:33 +0000

    Added Amplify

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,11 @@ import React from 'react';
 import './globals.css';
 import '@aws-amplify/ui-react/styles.css'
 import { Header, Footer } from '../components';
+import { Amplify } from 'aws-amplify';
+import outputs from '../../amplify_outputs.json';
+
+Amplify.configure(outputs, {ssr: true});
+
 
 
 export default function RootLayout({
